887c7dadd62cee7749e5108ef9107bb45a39ac2e
Kipper Klank <98369008+ProtogenKipper@users.noreply.github.com>
2022-11-22 18:58:52 -0600

    Add files via upload

---
+++ src/main/java/com/example/FXMLController.java
@@ -0,0 +1,46 @@
+package com.example;
+import java.sql.* ;
+/*
+Put header here
+
+
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+
+public class FXMLController implements Initializable {
+
+    
+    
+    @FXML
+    private Label lblOut;
+    
+    @FXML
+    private void btnClickAction(ActionEvent event) {
+        try {
+            Connection connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/new_database", "test", "password");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from products");
+
+            while (resultSet.next()){
+                lblOut.setText(resultSet.getString("name"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
